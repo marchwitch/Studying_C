0419f090b1517e4aecf2901adb023193c04b44a2
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-03 19:09:03 +0300

    add random

---
+++ Example005_Div_Random/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(1, 10); //1 2 3 4 5 6 7 8 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+double result = (numberA / numberB);
+Console.WriteLine(result);
