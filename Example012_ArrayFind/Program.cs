f5f6b0f22b2d07c3eec0cb20cd2f7103890205e0
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-06 11:36:53 +0300

    add example 012 find index in array

---
+++ Example012_ArrayFind/Program.cs
@@ -0,0 +1,18 @@
+﻿//
+
+int[] array = { 1, 12, 31, 18, 4, 15, 16, 17, 18 };
+
+int size = array.Length;
+int find = 18; // типа мы ищем, какую позицию (индекс) в массиве занимает число 4
+int index = 0;
+
+while (index < size)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++; //index = index + 1
+}
