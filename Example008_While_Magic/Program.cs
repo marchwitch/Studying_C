7bb519bbc94880401532dc9c85359e3bc084d3bf
Nadezhda Koroleva <marchwitch@gmail.com>
2022-09-29 13:25:59 +0300

    added some changes

--- Example008_While_Magic/Program.cs
+++ Example008_While_Magic/Program.cs
@@ -2,9 +2,9 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 40, ya = 1,
-    xb = 1, yb = 30,
-    xc = 80, yc = 30;
+int xa = 45, ya = 1,
+    xb = 1, yb = 35,
+    xc = 90, yc = 35;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
